7fbd85a9f445fbc1a85e1c6f1088ab7bb65a11ea
adamovivan <adamovivan@gmail.com>
2020-03-09 20:43:40 +0100

    Revert "Dockerized db, django app, elasticsearch, logstash, kibana, redis, nginx"

--- app/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# start django
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
-#python manage.py runserver 0.0.0.0:8000
-gunicorn app.wsgi -b 0.0.0.0:8000
