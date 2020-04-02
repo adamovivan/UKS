db8883c0c7ee7f111566f27157cc6b206e533568
Simona Prokic <167simonap@gmail.com>
2020-04-02 17:42:31 +0200

    Add tests for issue information

--- app/better_than_github/test/test_issues_views.py
+++ app/better_than_github/test/test_issues_views.py
@@ -32,6 +32,37 @@ class IssuesViewsTest(TestCase):
 
         issue.save()
 
+        assignee1 = User()
+        assignee1.name = "assignee1"
+        assignee1.email = "assignee1_email"
+        assignee1.save()
+
+        assignee2 = User()
+        assignee2.name = "assignee2"
+        assignee2.email = "assignee2_email"
+        assignee2.save()
+
+        label1 = Label()
+        label1.name = "wontfix"
+        label1.color = "purple"
+        label1.save()
+
+        label2 = Label()
+        label2.name = "feature"
+        label2.color = "blue"
+        label2.save()
+
+        milestone = Milestone()
+        milestone.title = "test milestone"
+        milestone.state = "OPEN"
+        milestone.description = "desc milestone test"
+        milestone.due_date = "2020-04-20"
+        milestone.project = project
+        milestone.open_issues = 0
+        milestone.closed_issues = 0
+        milestone.save()
+        
+
     def test_add_comment(self):
         issue_id = "2"
         comment_1_description = "Test comment 1"
@@ -55,3 +86,109 @@ class IssuesViewsTest(TestCase):
         self.assertTrue(comments[1].description == comment_2_description)
         self.assertTrue(comments[0].user.name == user)
         self.assertTrue(comments[1].user.name == user)
+
+    def test_add_assignees(self):
+        issue_id = "2"
+        assignees = ["assignee1", "assignee2"]
+        user = "test_user"
+
+        response = self.client.post('/repos/issue-assignees/add',
+                                     json.dumps({"issueId": issue_id, "assignees": assignees, "user": user}),
+                                     content_type="application/json")
+
+        self.assertTrue(response.status_code == 200)
+
+        responsibility_changes = ResponsibilityChange.objects.filter(issue=issue_id)
+        self.assertTrue(len(responsibility_changes) == 2)
+
+        self.assertTrue(responsibility_changes[0].responsible_user.name == "assignee1")
+        self.assertTrue(responsibility_changes[0].description == "added")
+        self.assertTrue(responsibility_changes[1].responsible_user.name == "assignee2")
+        self.assertTrue(responsibility_changes[1].description == "added")
+
+    def test_delete_assignee(self):
+        issue_id = "2"
+        assignee_name = "assignee1"
+        user = "test_user"
+
+        response = self.client.delete('/repos/issue-assignees/delete',
+                                     json.dumps({"issueId": issue_id, "assignee": assignee_name, "user": user}),
+                                     content_type="application/json")
+
+        self.assertTrue(response.status_code == 200)
+
+        responsibility_changes = ResponsibilityChange.objects.filter(issue=issue_id)
+        self.assertTrue(len(responsibility_changes) == 1)
+
+        self.assertTrue(responsibility_changes[0].responsible_user.name == "assignee1")
+        self.assertTrue(responsibility_changes[0].description == "removed")
+
+    def test_add_labels(self):
+        issue_id = "2"
+        labels = ["wontfix", "feature"]
+        user = "test_user"
+
+        response = self.client.post('/repos/issue-labels/add',
+                                     json.dumps({"issueId": issue_id, "labels": labels, "user": user}),
+                                     content_type="application/json")
+
+        self.assertTrue(response.status_code == 200)
+
+        label_changes = LabelChange.objects.filter(issue=issue_id)
+        self.assertTrue(len(label_changes) == 2)
+
+        self.assertTrue(label_changes[0].label_name == "wontfix")
+        self.assertTrue(label_changes[0].description == "added")
+        self.assertTrue(label_changes[1].label_name == "feature")
+        self.assertTrue(label_changes[1].description == "added")
+
+    def test_delete_label(self):
+        issue_id = "2"
+        label_name = "feature"
+        user = "test_user"
+
+        response = self.client.delete('/repos/issue-labels/delete',
+                                     json.dumps({"issueId": issue_id, "label": label_name, "user": user}),
+                                     content_type="application/json")
+
+        self.assertTrue(response.status_code == 200)
+
+        label_changes = LabelChange.objects.filter(issue=issue_id)
+        self.assertTrue(len(label_changes) == 1)
+
+        self.assertTrue(label_changes[0].label_name == "feature")
+        self.assertTrue(label_changes[0].description == "removed")
+
+    def test_add_milestone(self):
+        issue_id = "2"
+        milestone_title = "test milestone"
+        user = "test_user"
+
+        response = self.client.post('/repos/issue-milestone/add',
+                                     json.dumps({"issueId": issue_id, "milestone": milestone_title, "user": user}),
+                                     content_type="application/json")
+
+        self.assertTrue(response.status_code == 200)
+
+        milestone_change = MilestoneChange.objects.filter(issue=issue_id)
+        self.assertTrue(len(milestone_change) == 1)
+
+        self.assertTrue(milestone_change[0].milestone_title == "test milestone")
+        self.assertTrue(milestone_change[0].description == "added")
+
+    def test_delete_milestone(self):
+        issue_id = "2"
+        milestone_title = "test milestone"
+        user = "test_user"
+
+        response = self.client.delete('/repos/issue-milestone/delete',
+                                     json.dumps({"issueId": issue_id, "milestone": milestone_title, "user": user}),
+                                     content_type="application/json")
+
+        self.assertTrue(response.status_code == 200)
+
+        milestone_change = MilestoneChange.objects.filter(issue=issue_id)
+        self.assertTrue(len(milestone_change) == 1)
+
+        self.assertTrue(milestone_change[0].milestone_title == "test milestone")
+        self.assertTrue(milestone_change[0].description == "removed")
